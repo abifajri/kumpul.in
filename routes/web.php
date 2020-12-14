1124f9a2547e6f74fcb98b1d7a28115bd66c8062
Abi Fajri <45683007+abifajri@users.noreply.github.com>
2020-12-14 22:09:57 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,17 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
 Route::get('/chat', [App\Http\Controllers\ChatController::class, 'index'])->name('chat');
-
 
 Route::get('/groups/create', function() {
     return view('groups.create');
@@ -37,9 +32,8 @@ Route::get('/room/edit', function(){
     return view('room.edit');
 })->name('room.edit');
 
-
+Route::get('/', [App\Http\Controllers\TimelineController::class, 'index'])->name('timeline');
 Route::get('/timeline', [App\Http\Controllers\TimelineController::class, 'index'])->name('timeline');
-
 Route::get('/view_profile', [App\Http\Controllers\ProfileController::class, 'view_index'])->name('view_profile');
 Route::get('/edit_profile', [App\Http\Controllers\ProfileController::class, 'edit_index'])->name('edit_profile');
 Route::get('/room', [App\Http\Controllers\RoomController::class, 'index'])->name('room');
