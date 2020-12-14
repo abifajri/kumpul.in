0352b45a3ce615509d63254bba0112c0cdffd741
Adriel Ananta <adriel.gian@ui.ac.id>
2020-12-14 15:00:52 +0700

    create group page done; group page init

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/groups/create', function() {
+    return view('groups.create');
+})->name('gorups.create');
+
+Route::get('/groups', function() {
+    return view('groups.index');
+})->name('groups.index');
