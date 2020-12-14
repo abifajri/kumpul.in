51ec6045a6d8acee80b1be292de3bee631a0548c
Adriel Ananta <adriel.gian@ui.ac.id>
2020-12-14 19:50:40 +0700

    add group index page

--- resources/views/groups/index.blade.php
+++ resources/views/groups/index.blade.php
@@ -1,7 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    
+<div class="container text-center">
+    <div class="row justify-content-center">
+        <div class="col-sm-7">
+            
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="card">
+                 <p>John</p>
+                 <img src="default.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                </div>
+              </div>
+              <div class="col-sm-9">
+                <div class="card">
+                  <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="card">
+                 <p>Bo</p>
+                 <img src="default.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                </div>
+              </div>
+              <div class="col-sm-9">
+                <div class="card">
+                  <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="card">
+                 <p>Jane</p>
+                 <img src="default.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                </div>
+              </div>
+              <div class="col-sm-9">
+                <div class="card">
+                  <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-sm-3">
+                <div class="card">
+                 <p>Anja</p>
+                 <img src="{{asset('/images/default.jpg')}}" class="img-circle" height="55" width="55" alt="Avatar">
+                </div>
+              </div>
+              <div class="col-sm-9">
+                <div class="card">
+                  <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+                </div>
+              </div>
+            </div>     
+          </div>
+    </div>
 </div>
 @endsection
