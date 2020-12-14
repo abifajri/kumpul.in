3f68701864b546af72ee30f942080ef0e3dc2478
Adriel Ananta <adriel.gian@ui.ac.id>
2020-12-14 21:11:32 +0700

    merge route

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,6 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-<<<<<<< HEAD
 Route::get('/groups/create', function() {
     return view('groups.create');
 })->name('gorups.create');
@@ -29,14 +28,12 @@ Route::get('/groups/create', function() {
 Route::get('/groups', function() {
     return view('groups.index');
 })->name('groups.index');
-=======
 
 Route::get('/room/edit', function(){
     return view('room.edit');
 })->name('room.edit');
 
 Route::get('/timeline', [App\Http\Controllers\TimelineController::class, 'index'])->name('timeline');
->>>>>>> ca1ba0f1056f9a2522dd3eb57e09da0a13ce3715
 Route::get('/view_profile', [App\Http\Controllers\ProfileController::class, 'view_index'])->name('view_profile');
 Route::get('/edit_profile', [App\Http\Controllers\ProfileController::class, 'edit_index'])->name('edit_profile');
 Route::get('/room', [App\Http\Controllers\RoomController::class, 'index'])->name('room');
