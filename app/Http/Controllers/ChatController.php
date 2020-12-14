7cd6eb5b68e00c9a293a5586bd8dc801abd5012d
unknown <mdahlany@gmail.com>
2020-12-14 21:47:18 +0700

    Front-end feature chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('chat');
+    }    
 }
